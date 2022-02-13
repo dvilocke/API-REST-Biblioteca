938e492ce9a7a2a1281188763c0487e1b0e8ff6a
Miguel <miguel.ramirez@ohmyfi.com>
2022-02-13 16:12:34 -0500

    creacion de la clase BD

---
+++ BD.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * database implementation
+ */
+
+const DNS = 'mysql:host=localhost;dbname=bdbiblioteca';
+const USUARIO = 'root';
+const PASSWORD = '';
+
+class BD
+{
+    private $bd;
+
+    public function __construct($dsn, $username, $password)
+    {
+        try
+        {
+            $this->bd = new PDO($dsn, $username, $password);
+            $this->bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        }catch (PDOException $e)
+        {
+            echo('Error:' . $e->getMessage());
+            die();
+        }
+
+    }
+}
+
+
